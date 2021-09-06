3a1ba1dc6c7faffd243b164d03914ff067624a72
akshada4 <akshadasipsap@gmail.com>
2021-09-06 22:26:45 +0530

    get by id

--- fruitsAPI.py
+++ fruitsAPI.py
@@ -3,11 +3,25 @@ import json
 
 app = FastAPI()
 
-@app.get("/")
-def send_data():
+def read_json():
 	f = open('data.json')
-	data = json.load(f)
-	return data 
+	return json.load(f)
+
+@app.get("/")
+def get_data():
+	data = read_json()
+	return data
+
+@app.get("/{id}")
+def get_data(id: int):
+	data = read_json()
+	for i in data:
+		if i["id"] == id:
+			return i
+		else:
+			return {}
+
+
 
 if __name__ == "__main__":
 	uvicorn.run("main:app")
