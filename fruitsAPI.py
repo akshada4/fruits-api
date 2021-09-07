9f398f441e448157b1200ae01a2936a79da4796f
akshada4 <akshadasipsap@gmail.com>
2021-09-07 23:26:06 +0530

    added status and removed some weird code lmao

--- fruitsAPI.py
+++ fruitsAPI.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 import json
 
 app = FastAPI()
@@ -7,20 +7,20 @@ def read_json():
 	f = open('data.json')
 	return json.load(f)
 
-@app.get("/")
+@app.get("/",status_code=200)
 def get_data():
 	data = read_json()
 	return data
 
-@app.get("/{id}")
+@app.get("/{id}",status_code=200)
 def get_data(id: int):
 	data = read_json()
+
 	for i in data:
 		if i["id"] == id:
 			return i
-		else:
-			return {}
 
+	raise HTTPException(status_code=404, detail="item not found")
 
 
 if __name__ == "__main__":
